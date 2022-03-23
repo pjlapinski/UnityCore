9826f74ea511369f8af57fa52f5272689ceb55d2
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2022-03-23 10:17:29 +0100

    Updated the SetFlag method for a more performant implementation

--- Utilities/Extensions/EnumExtensions.cs
+++ Utilities/Extensions/EnumExtensions.cs
@@ -1,26 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace PJL.Utilities.Extensions
 {
     public static class EnumExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void SetFlag<T>(ref this T @enum, T flag, bool value) where T : struct, Enum
         {
-            // Because the underlying type can be a byte, sbyte, short, ushort, int, uint, long or ulong
-            // the method cannot be generic without the use of the 'dynamic' type
-            var tType = Enum.GetUnderlyingType(typeof(T));
-            dynamic enumValue = Convert.ChangeType(@enum, tType);
-            dynamic flagValue = Convert.ChangeType(flag, tType);
-
-            if (value)
+            var enumSize = UnsafeUtility.SizeOf<T>();
+            if (enumSize == 1)
             {
-                @enum = (T)Enum.ToObject(typeof(T), enumValue | flagValue);
-                return;
+                var newValue = (byte)(value
+                    ? UnsafeUtility.As<T, byte>(ref @enum) | UnsafeUtility.As<T, byte>(ref flag)
+                    : UnsafeUtility.As<T, byte>(ref @enum) & ~UnsafeUtility.As<T, byte>(ref flag));
+                @enum = UnsafeUtility.As<byte, T>(ref newValue);
             }
-
-            if (@enum.HasFlag(flag))
+            else if (enumSize == 2)
             {
-                @enum = (T)Enum.ToObject(typeof(T), enumValue & ~flagValue);
+                var newValue = (short)(value
+                    ? UnsafeUtility.As<T, short>(ref @enum) | UnsafeUtility.As<T, short>(ref flag)
+                    : UnsafeUtility.As<T, short>(ref @enum) & ~UnsafeUtility.As<T, short>(ref flag));
+                @enum = UnsafeUtility.As<short, T>(ref newValue);
+            }
+            else if (enumSize == 4)
+            {
+                var newValue = value
+                    ? UnsafeUtility.As<T, uint>(ref @enum) | UnsafeUtility.As<T, uint>(ref flag)
+                    : UnsafeUtility.As<T, uint>(ref @enum) & ~UnsafeUtility.As<T, uint>(ref flag);
+                @enum = UnsafeUtility.As<uint, T>(ref newValue);
+            }
+            else
+            {
+                var newValue = value
+                    ? UnsafeUtility.As<T, ulong>(ref @enum) | UnsafeUtility.As<T, ulong>(ref flag)
+                    : UnsafeUtility.As<T, ulong>(ref @enum) & ~UnsafeUtility.As<T, ulong>(ref flag);
+                @enum = UnsafeUtility.As<ulong, T>(ref newValue);
             }
         }
     }
