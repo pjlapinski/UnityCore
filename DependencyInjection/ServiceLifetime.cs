298cbea9db32b35c5b7f1dc8b6e426cff5d8fc5a
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2022-06-16 00:15:08 +0200

    Added a DI container implementation

---
+++ DependencyInjection/ServiceLifetime.cs
@@ -0,0 +1,11 @@
+namespace PJL.DependencyInjection
+{
+    /// <summary>
+    /// Determines whether a service will be a singleton or transient
+    /// </summary>
+    public enum ServiceLifetime
+    {
+        Singleton,
+        Transient,
+    }
+}
