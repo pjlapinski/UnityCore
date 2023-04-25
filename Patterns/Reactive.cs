713c73df1cda76df1af5ab72888c0feae9ce8528
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-04-25 02:08:50 +0200

    Created a reactive property

---
+++ Patterns/Reactive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PJL.Patterns
+{
+    [Serializable]
+    public struct Reactive<T>
+    {
+        [SerializeField] private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnValueChange?.Invoke(_value);
+            }
+        }
+
+        [field: SerializeField] public UnityEvent<T> OnValueChange { get; private set; }
+    }
+}
