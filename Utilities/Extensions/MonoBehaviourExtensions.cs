87433616ae582e8a12448b369fc9c0f6cc39589b
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-05-28 00:58:52 +0200

    Added logging extensions to monobehaviours

--- Utilities/Extensions/MonoBehaviourExtensions.cs
+++ Utilities/Extensions/MonoBehaviourExtensions.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using PJL.Logging;
+using UnityEngine;
 
 namespace PJL.Utilities.Extensions
 {
     public static class MonoBehaviourExtensions
     {
         public static void DestroyObject(this MonoBehaviour mono) => Object.Destroy(mono.gameObject);
+
+        public static void Log(this MonoBehaviour _, Severity severity, string context, object message) =>
+            ContextLogger.Log(severity, context, message);
+
+        public static void LogFmt(this MonoBehaviour _, Severity severity, string context, string format, params object[] insertions) =>
+            ContextLogger.LogFormat(severity, context, format, insertions);
     }
 }
