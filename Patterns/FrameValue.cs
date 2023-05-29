be32eebf8c86589c0262bca5836807712a622b1c
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-05-29 21:42:49 +0200

    FrameValue and WaitForFrames added

---
+++ Patterns/FrameValue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PJL.Patterns
+{
+    public readonly struct FrameValue<T>
+    {
+        public Option<T> Value { get; }
+
+        public FrameValue(T value, MonoBehaviour reference)
+        {
+            Value = value;
+            reference.StartCoroutine(InvalidateAfterFrame());
+        }
+
+        private IEnumerator InvalidateAfterFrame()
+        {
+            yield return null;
+            Value.SetNone();
+        }
+    }
+}
---
+++ Utilities/Coroutines/WaitForFrames.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PJL.Utilities.Coroutines
+{
+    public class WaitForFrames : CustomYieldInstruction
+    {
+        private int _framesAfterDelay;
+
+        public override bool keepWaiting => Time.frameCount < _framesAfterDelay;
+
+        public WaitForFrames(int frames)
+        {
+            _framesAfterDelay = Time.frameCount + frames;
+        }
+    }
+}
