5661efeb8287555e88914b3a341c47916e1d1f06
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2026-01-13 16:30:59 +0100

    Added NextFrame extension for MonoBehaviours

---
+++ Utilities/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PJL.Utilities.Extensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static void NextFrame(this MonoBehaviour mono, Action callback) => mono.StartCoroutine(NextFrameCo(callback));
+
+        private static IEnumerator NextFrameCo(Action callback)
+        {
+            yield return null;
+            callback();
+        }
+    }
+}
