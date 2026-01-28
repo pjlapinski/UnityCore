b8f5d4d7fd31e6e78080638976c3f594c1deac2b
plapinski <plapinski@gwo.pl>
2026-01-28 13:59:18 +0100

    Added input handling

---
+++ Input/InputHandler.cs
@@ -0,0 +1,52 @@
+﻿using PJL.Patterns;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PJL.Input
+{
+    public class InputHandler : Singleton<InputHandler>
+    {
+        [SerializeField] internal PlayerInput PlayerInput;
+        [SerializeField] private InputReceiver _activeReceiver;
+
+        public static InputReceiver ActiveReceiver
+        {
+            get => Instance._activeReceiver;
+            set
+            {
+                if (Instance._activeReceiver != null)
+                    Instance._activeReceiver.OnBecomeInactive.Invoke();
+                Instance._activeReceiver = value;
+                Instance._activeReceiver.OnBecomeActive.Invoke();
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if (_activeReceiver != null)
+                _activeReceiver.OnBecomeActive.Invoke();
+            foreach (var e in PlayerInput.actionEvents)
+                e.AddListener(Handle);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (_activeReceiver != null)
+                _activeReceiver.OnBecomeInactive.Invoke();
+            foreach (var e in PlayerInput.actionEvents)
+                e.RemoveListener(Handle);
+        }
+
+        private void Handle(InputAction.CallbackContext context)
+        {
+            if (ActiveReceiver == null) return;
+
+            var action = $"{context.action.actionMap.name}.{context.action.name}";
+            foreach (var cb in ActiveReceiver.ActionCallbacks)
+                if (cb.Action == action)
+                    cb.Callback.Invoke(context);
+        }
+    }
+}
---
+++ Input/InputReceiver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace PJL.Input
+{
+    [Serializable]
+    internal class InputCallback
+    {
+#if UNITY_EDITOR
+        private DropdownList<string> InputActions()
+        {
+            var list = new DropdownList<string>();
+            foreach (var action in InputHandler.Instance.PlayerInput.actions)
+            {
+                var val = action.actionMap.name + "." + action.name;
+                list.Add(val, val);
+            }
+
+            return list;
+        }
+
+        [Dropdown(nameof(InputActions))]
+#endif
+        public string Action;
+        public UnityEvent<InputAction.CallbackContext> Callback;
+    }
+
+    public class InputReceiver : MonoBehaviour
+    {
+        [SerializeField] internal InputCallback[] ActionCallbacks;
+        [field: SerializeField] public UnityEvent OnBecomeActive { get; set; }
+        [field: SerializeField] public UnityEvent OnBecomeInactive { get; set; }
+    }
+}
