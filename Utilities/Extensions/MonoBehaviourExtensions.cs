9ed83674d75d66c87fa0f626d3e557fc3aaa63cc
plapinski <plapinski@gwo.pl>
2026-05-06 10:40:38 +0200

    Added more Mono extensions

--- Utilities/Extensions/MonoBehaviourExtensions.cs
+++ Utilities/Extensions/MonoBehaviourExtensions.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections;
+using PJL.Utilities.Coroutines;
 using UnityEngine;
 
 namespace PJL.Utilities.Extensions
 {
     public static class MonoBehaviourExtensions
     {
-        public static void NextFrame(this MonoBehaviour mono, Action callback) => mono.StartCoroutine(NextFrameCo(callback));
+        public static void AfterSeconds(this MonoBehaviour mono, Action callback, float seconds) => 
+            mono.StartCoroutine(AfterSecondsCo(callback, seconds));
 
-        private static IEnumerator NextFrameCo(Action callback)
+        public static void AfterFrames(this MonoBehaviour mono, Action callback, int frames) => 
+            mono.StartCoroutine(AfterFramesCo(callback, frames));
+
+        public static void NextFrame(this MonoBehaviour mono, Action callback) => 
+            mono.StartCoroutine(AfterFramesCo(callback, 1));
+
+        private static IEnumerator AfterSecondsCo(Action callback, float seconds)
         {
-            yield return null;
+            yield return WaitFor.Seconds(seconds);
+            callback();
+        }
+
+        private static IEnumerator AfterFramesCo(Action callback, int frames)
+        {
+            yield return WaitFor.Frames(frames);
             callback();
         }
     }
